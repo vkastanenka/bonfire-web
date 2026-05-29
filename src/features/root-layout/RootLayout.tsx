4533fa214efac82b8548366c770d072a12b08915
Victoria Kastanenka <vkastanenka@gmail.com>
2026-05-28 21:43:03 -0400

    updated implementation

--- src/features/root-layout/RootLayout.tsx
+++ src/features/root-layout/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "@tanstack/react-router";
+import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -12,15 +12,6 @@ const theme = createTheme({
 export const RootLayout = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{" "}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
     <Outlet />
     <TanStackRouterDevtools />
   </ThemeProvider>
